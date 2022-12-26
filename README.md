c8b6244bbb14a66f638be9398ae2168efabc1b31
Sergey Trofimov <github@sergey.trofimov.name>
2022-12-26 16:13:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PROJECT_NAME
+
+PROJECT_DESCRIPTION
+
+[![cljdoc badge](https://cljdoc.org/badge/com.github.strojure/PROJECT_NAME)](https://cljdoc.org/d/com.github.strojure/PROJECT_NAME)
+[![Clojars Project](https://img.shields.io/clojars/v/com.github.strojure/PROJECT_NAME.svg)](https://clojars.org/com.github.strojure/PROJECT_NAME)
+
+## Motivation
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.github.strojure/PROJECT_NAME "1.0.0-SNAPSHOT"
+  :description "PROJECT_DESCRIPTION"
+  :url "https://github.com/strojure/PROJECT_NAME"
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies []
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}}
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]])
