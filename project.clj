724622f809a7a0a931af38f4ea5f1c2f92f3fbd1
serioga <github@sergey.trofimov.name>
2023-01-07 12:44:52 +0100

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/ring-control "1.0.48-beta2"
+(defproject com.github.strojure/ring-control "1.0.49-SNAPSHOT"
   :description "More controllable composition of Ring middlewares."
   :url "https://github.com/strojure/ring-control"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
